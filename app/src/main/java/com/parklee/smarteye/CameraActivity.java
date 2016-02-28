10d878d5b8471f6df687ed019af44948e8a2f0fa
ParkLee <739830473@qq.com>
2016-02-28 21:03:02 +0800

    新界面！

--- app/src/main/java/com/parklee/smarteye/CameraActivity.java
+++ app/src/main/java/com/parklee/smarteye/CameraActivity.java
@@ -1,65 +1,124 @@
 package com.parklee.smarteye;
 /**
  * 拍照界面
- * 1、无ActionBar
- * 2、单张、多张
+ * 1、OK 无ActionBar
+ * 2、单张、多张、闪关灯按钮
  * 3、拍照
- * 4、单张模式下，点击拍照进入ResultActivity
- * 5、多张模式下，继续拍照
- *
- * 拍照已实现
+ * 4、单张模式下，点击拍照进入ResultActivity,保存到默认保存路径（这个没实现）
+ * 5、多张模式下，继续拍照，保存到默认保存路径（这个没实现）
+ * 6、闪光灯 （杰锅鼓捣了一下，失败）
+ * 7、网格 （不会）
  */
+
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.ImageFormat;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+import android.hardware.Camera.Parameters;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 public class CameraActivity extends Activity implements SurfaceHolder.Callback {
 
     private Camera mCamera;
     private SurfaceView mPreview;
     private SurfaceHolder mHolder;
+    private String nameForDir;
+    private String TAG = "info";
+    private int moreNum = 0;
+    private RadioGroup group;
+    private int flag=0;
+    private ImageButton btn_flash= (ImageButton) findViewById(R.id.btn_flash);
     private Camera.PictureCallback mPictureCallback = new Camera.PictureCallback() {
         @Override
         public void onPictureTaken(byte[] data, Camera camera) {
             try {
-                Date date = new Date();
-                SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
-                String filename = format.format(date) + ".jpg";
-                File fileFolder = new File(Environment.getExternalStorageDirectory() + "/SEPic/");
-                if (!fileFolder.exists()) {
-                    fileFolder.mkdir();
+                group = (RadioGroup) findViewById(R.id.choose_model);
+                int choosed = group.getCheckedRadioButtonId();
+                if (choosed == R.id.one) {
+                    Date date = new Date();
+                    SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
+                    String filename = format.format(date) + ".jpg";
+                    File fileFolder = new
+                            File(Environment.getExternalStorageDirectory() + "/SmartEye/" + nameForDir);
+                    if (!fileFolder.exists()) {
+                        fileFolder.mkdir();
+                    }
+                    File jpgFile = new File(fileFolder, filename);
+                    FileOutputStream fos = new FileOutputStream(jpgFile);
+                    fos.write(data);
+                    fos.close();
+
+                    Intent intent = new Intent(CameraActivity.this, ResultActivity.class);
+                    intent.putExtra("picPath", jpgFile.getAbsolutePath());
+                    startActivity(intent);
+                    CameraActivity.this.finish();
+                } else if (choosed == R.id.more) {
+                    if (moreNum == 0) {
+                        RelativeLayout insertLayout = (RelativeLayout) findViewById(R.id.camera_layout);
+                        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(100, 100);
+                        layoutParams.topMargin = 8;
+                        layoutParams.leftMargin = 8;
+                        layoutParams.rightMargin = 8;
+                        layoutParams.bottomMargin = 8;
+                        layoutParams.addRule(RelativeLayout.RIGHT_OF, R.id.btn_capture);
+                        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+
+                        Button text = new Button(CameraActivity.this);
+                        text.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                        text.setText("完成");
+                        text.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View arg0) {
+                                startActivity(new Intent(CameraActivity.this, MainActivity.class));
+                            }
+                        });
+
+                        insertLayout.addView(text, layoutParams);
+                    }
+                    Date date = new Date();
+                    SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
+                    String filename = format.format(date) + ".jpg";
+                    File fileFolder = new
+                            File(Environment.getExternalStorageDirectory() + "/SmartEye/" + nameForDir);
+                    if (!fileFolder.exists()) {
+                        fileFolder.mkdir();
+                    }
+                    File jpgFile = new File(fileFolder, filename);
+                    FileOutputStream fos = new FileOutputStream(jpgFile);
+                    fos.write(data);
+                    fos.close();
+                    moreNum++;
+                    mCamera.startPreview();
                 }
-                File jpgFile = new File(fileFolder, filename);
-                FileOutputStream fos = new FileOutputStream(jpgFile);
-                fos.write(data);
-                fos.close();
-                Intent intent = new Intent(CameraActivity.this, ResultActivity.class);
-                intent.putExtra("picPath", jpgFile.getAbsolutePath());
-                startActivity(intent);
-                CameraActivity.this.finish();
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     };
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        Intent i = getIntent();
+        nameForDir = i.getStringExtra("nameForDir");
         mPreview = (SurfaceView) findViewById(R.id.preview);
         mHolder = mPreview.getHolder();
         mHolder.setKeepScreenOn(true);
@@ -68,6 +127,13 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback {
             @Override
             public void onClick(View v) {
                 mCamera.autoFocus(null);
+            }
+        });
+        Button capture = (Button) findViewById(R.id.btn_capture);
+        capture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                mCamera.takePicture(null, null, mPictureCallback);
             }
         });
     }
@@ -87,6 +153,71 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback {
             }
         });
     }
+
+    public void flashcontrol(View view){
+        if(flag==0){
+            turnLightOn(getCamera());
+            btn_flash.setImageResource(R.mipmap.btn_ic_flashon);
+            flag=1;
+        }else if(flag==1){
+            turnLightOff(getCamera());
+            btn_flash.setImageResource(R.mipmap.btn_ic_flashoff);
+            flag=0;
+        }
+    }
+
+    private void turnLightOn(Camera camera) {
+        if (mCamera == null) {
+            return;
+            }
+        Parameters parameters = mCamera.getParameters();
+        if (parameters == null) {
+            return;
+            }
+        List<String> flashModes = parameters.getSupportedFlashModes();
+             // Check if camera flash exists
+            if (flashModes == null) {
+                  // Use the screen as a flashlight (next best thing)
+                  return;
+                }
+             String flashMode = parameters.getFlashMode();
+            if (!Parameters.FLASH_MODE_TORCH.equals(flashMode)) {
+                    // Turn on the flash
+                    if (flashModes.contains(Parameters.FLASH_MODE_TORCH)) {
+                            parameters.setFlashMode(Parameters.FLASH_MODE_TORCH);
+                            mCamera.setParameters(parameters);
+                        } else {
+                         }
+                }
+
+    }
+
+    private void turnLightOff(Camera camera) {
+        if (mCamera == null) {
+            return;
+             }
+         Parameters parameters = mCamera.getParameters();
+         if (parameters == null) {
+            return;
+            }
+         List<String> flashModes = parameters.getSupportedFlashModes();
+         String flashMode = parameters.getFlashMode();
+         // Check if camera flash exists
+         if (flashModes == null) {
+        return;
+        }
+         if (!Parameters.FLASH_MODE_OFF.equals(flashMode)) {
+             // Turn off the flash
+            if (flashModes.contains(Parameters.FLASH_MODE_OFF)) {
+                parameters.setFlashMode(Parameters.FLASH_MODE_OFF);
+                mCamera.setParameters(parameters);
+                } else {
+                Log.e(TAG, "FLASH_MODE_OFF not supported");
+            }
+        }
+
+    }
+
 
     @Override
     protected void onResume() {
--- app/src/main/java/com/parklee/smarteye/MainActivity.java
+++ app/src/main/java/com/parklee/smarteye/MainActivity.java
@@ -1,30 +1,227 @@
 package com.parklee.smarteye;
 /**
  * 主界面
- * 1、一个ActionBar，右上角一个菜单（重命名、删除、关于我们等）
- * 2、文档的图片和名字显示，初定GridLayout 不确定
- * 3、两个按钮，拍照和导入
+ * 1、OK 一个ActionBar，，搜索+右上角一个菜单（重命名、删除、教程、合并、关于我们等）
+ * 2、文档的图片和名字显示，已用ListView，最好能显示缩略图，左边缩略图，右边文件名
+ * 3、OK 两个按钮，拍照和导入，FloatingActionButton
  * 4、点击拍照，进入CameraActivity
  * 5、点击导入，进行导入
- * 6、点击文档，进入文档浏览界面
- *
- * 拍照已实现
+ * 6、点击文档，进入ExplorerActivity
+ * 7、搜索
+ * 8、右上角一个菜单（重命名、删除、教程、合并、关于我们等）里面的功能，长摁也可唤出
  */
-import android.app.Activity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String nameForNewDir;
+    private ListView listview;
+    public int MID;
+    public String dirName;
+    public String sdCard;
+    private String TAG = "info";
+    List<Map<String, Object>> listItems = new ArrayList<Map<String, Object>>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sdCard = getSDPath();
+        if (sdCard == null) {
+            new AlertDialog.Builder(this)
+                    .setTitle("提示")
+                    .setMessage("无法读取内存卡")
+                    .setPositiveButton("确定", null)
+                    .show();
+        } else {
+            init(sdCard);
+            listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view,
+                                        int position, long id) {
+                    Map<String, Object> list = listItems.get(position);
+                    dirName = (String) list.get("fileName");
+                    Intent intent = new Intent(MainActivity.this, ExplorerActivity.class);
+                    intent.putExtra("dirName", dirName);
+                    startActivity(intent);
+                }
+            });
+            //为每个listview的item设置长按相应菜单
+            listview.setOnCreateContextMenuListener(new View.OnCreateContextMenuListener() {
+                @Override
+                public void onCreateContextMenu(ContextMenu menu, View v,
+                                                ContextMenu.ContextMenuInfo menuInfo) {
+                    menu.add(0, 0, 0, "删除");
+                    menu.add(0, 1, 0, "编辑");
+                    final AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+                    View view = info.targetView;
+                    TextView nameText = (TextView) view.findViewById(R.id.file_name);
+                    dirName = (String) nameText.getText();
+                }
+            });
+        }
+    }
+
+    //作为长按后弹出来的菜单选择后做出的响应
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case 0:
+                deleteDir(sdCard + "/SmartEye/" + dirName);
+                break;
+            case 1:
+                changeDirName(sdCard + "/SmartEye/" + dirName);
+                break;
+            default:
+                break;
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    //公用name_input.xml作为输入新名字的框
+    public void changeDirName(String dirPath) {
+        LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+        final View dialogView = inflater.inflate(R.layout.filename_input, null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setView(dialogView)
+                .setTitle("请输入更改后文件夹名字")
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        EditText name = (EditText) dialogView.findViewById(R.id.et_filename);
+                        nameForNewDir = name.getText().toString();
+                        File desFileDir = new File(sdCard + "/SmartEye/" + nameForNewDir);
+                        if (!desFileDir.exists()) {
+                            File srcDirFile = new File(sdCard + "/SmartEye/" + dirName);
+                            srcDirFile.renameTo(desFileDir);
+                            init(sdCard);
+                        } else {
+                            new AlertDialog.Builder(MainActivity.this)
+                                    .setTitle("提示")
+                                    .setMessage("该名字文件夹已存在")
+                                    .setPositiveButton("确定", null)
+                                    .show();
+                        }
+                    }
+                }).show();
+    }
+
+    //删除指定目录的文件夹及下面的文件
+    public void deleteDir(String dirPath) {
+        File dirFile = new File(dirPath);
+        File[] downFile = dirFile.listFiles();
+        for (int i = 0; i < downFile.length; i++) {
+            downFile[i].delete();
+        }
+        dirFile.delete();
+        init(sdCard);
+    }
+
+    //实现了从sdcard/SmartEye下面读取文件夹，将其以listview的item形式显示出来
+    public void init(String sdCard) {
+        File picData = new File(sdCard + "/SmartEye");
+        if (!picData.exists())
+            picData.mkdir();
+        String[] fileName = picData.list();
+        String test = "";
+        for (int i = 0; i < fileName.length; i++) {
+            test += fileName[i] + ";";
+        }
+        listview = (ListView) findViewById(R.id.list_view);
+        for (int i = 0; i < fileName.length; i++) {
+            Map<String, Object> listItem = new HashMap<String, Object>();
+            listItem.put("fileName", fileName[i]);
+            listItems.add(listItem);
+        }
+        SimpleAdapter simpleAdapter = new SimpleAdapter(this, listItems, R.layout.main_list_item,
+                new String[]{"fileName"},
+                new int[]{R.id.file_name});
+        listview.setAdapter(simpleAdapter);
+    }
+
+    public String getSDPath() {
+        File sdDir = null;
+        boolean sdCardExist = Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED);
+        if (sdCardExist) {
+            sdDir = Environment.getExternalStorageDirectory();
+        }
+        return sdDir.toString();
     }
 
     public void StartCamera(View view) {
-        startActivity(new Intent(this, CameraActivity.class));
+        LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+        final View dialogView = inflater.inflate(R.layout.filename_input, null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setView(dialogView)
+                .setTitle("请输入新建文件夹名字")
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        EditText name = (EditText) dialogView.findViewById(R.id.et_filename);
+                        nameForNewDir = name.getText().toString();
+                        File newDirFile = new File(sdCard + "/SmartEye/" + nameForNewDir);
+                        if (newDirFile.exists()) {
+                            new AlertDialog.Builder(MainActivity.this)
+                                    .setTitle("提示")
+                                    .setMessage("该名字文件夹已存在")
+                                    .setPositiveButton("确定", null)
+                                    .show();
+                        } else {
+                            Intent intent = new Intent(MainActivity.this, CameraActivity.class);
+                            intent.putExtra("nameForDir", nameForNewDir);
+                            startActivity(intent);
+                        }
+                    }
+                }).show();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_actionbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                openSearch();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void openSearch() {
+
+    }
+
 
 }
